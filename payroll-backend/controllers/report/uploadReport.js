c09c69d4f15c2b73516552e6732beaf7d9748469
G <igauravsehrawat@gmail.com>
2018-09-27 20:57:33 +0530

    DB insertion.
    Saving the uploaded report in the DB.

--- payroll-backend/controllers/report/uploadReport.js
+++ payroll-backend/controllers/report/uploadReport.js
@@ -1,15 +1,40 @@
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const csvParser = Promise.promisify(require('csv-parse'));
+const moment = require('moment');
+
+const WorkLog = require('../../models/WorkLog');
 
 const csvParserOptions = {
   ltrim: true,
 };
 
+const docsToInsert = async (parsedCSV) => {
+  const rowsCount = parsedCSV.length;
+  const lastRowIndex = rowsCount - 1;
+  const penultimateRowIndex = rowsCount - 2;
+  const reportId = parsedCSV[lastRowIndex][1];
+  const docs = [];
+  for (let index = 1; index <= penultimateRowIndex; index += 1) {
+    const currentRow = parsedCSV[index];
+    const doc = {
+      date: moment(currentRow[0], 'DD/MM/YYYY').utc().format(),
+      hoursWorked: currentRow[1],
+      employeeId: currentRow[2],
+      jobGroup: currentRow[3],
+      reportId,
+    };
+    docs.push(doc);
+  }
+  await WorkLog.insertMany(docs);
+  return docs;
+};
+
 const uploadReport = async (req, res) => {
   const fileContent = await fs.readFileAsync(req.file.path, 'utf8');
   const parsedCSV = await csvParser(fileContent, csvParserOptions);
-  res.status(200).send(parsedCSV);
+  const docs = await docsToInsert(parsedCSV);
+  res.status(200).send(docs);
 };
 
 module.exports = uploadReport;
