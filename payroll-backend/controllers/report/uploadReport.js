1c1d4b80463c09162dd6126bca3ed812c3ffd8b5
G <igauravsehrawat@gmail.com>
2018-09-27 09:09:56 +0530

    Converted callback based API to be Promise based using bluebird.

--- payroll-backend/controllers/report/uploadReport.js
+++ payroll-backend/controllers/report/uploadReport.js
@@ -1,5 +1,15 @@
-const uploadReport = (req, res) => {
-  res.status(200).send('To be implemented.');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const csvParser = Promise.promisify(require('csv-parse'));
+
+const csvParserOptions = {
+  ltrim: true,
+};
+
+const uploadReport = async (req, res) => {
+  const fileContent = await fs.readFileAsync(req.file.path, 'utf8');
+  const parsedCSV = await csvParser(fileContent, csvParserOptions);
+  res.status(200).send(parsedCSV);
 };
 
 module.exports = uploadReport;
