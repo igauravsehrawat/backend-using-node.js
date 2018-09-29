d2dca7a131b72eb5fe0103881ea3dde3c5b07aee
G <igauravsehrawat@gmail.com>
2018-09-29 13:29:49 +0530

    Docs for expressErrorHandler.

--- payroll-backend/services/errorHandlers/index.js
+++ payroll-backend/services/errorHandlers/index.js
@@ -1,3 +1,13 @@
+
+
+/**
+ * This is central function to catch error from async function
+ * Instead of writing try...catch block for each async,
+ * this utility can be used to wrap it and use as normal function.
+ *
+ * req, res, next are passed to fn, which are the controller arguments
+ * @param {function} fn express route controller
+ */
 const expressErrorHandler = fn => (req, res, next) => fn(req, res, next).catch((err) => {
   next({ err });
 });
