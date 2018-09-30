cca0a07c7f81c9fbe3782777b94b8ddd801c1b98
G <igauravsehrawat@gmail.com>
2018-09-30 12:28:58 +0530

    --add
    prerequisite data script for job group rates.

---
+++ payroll-backend/bin/prefillJobGroupRates.js
@@ -0,0 +1,42 @@
+const dotenvResult = require('dotenv').config();
+
+if (dotenvResult.error) {
+  console.error('Can not start the app without env variables');
+  process.exit();
+}
+
+const JobGrouRate = require('../models/JobGroupRate');
+
+
+const prefillDataForJobRates = async () => {
+  const jobDocs = [];
+  const groupAJob = {
+    jobGroup: 'A',
+    perHourRate: 20,
+    currency: 'CAD',
+  };
+  const groupBJob = {
+    jobGroup: 'B',
+    perHourRate: 30,
+    currency: 'CAD',
+  };
+  jobDocs.push(groupAJob);
+  jobDocs.push(groupBJob);
+  const existingDocs = await JobGrouRate.find({}).lean().exec();
+  if (existingDocs.length > 0) {
+    console.info('Seems like there is data already. Exiting out.');
+    process.exit();
+  }
+  const insertedDocs = await JobGrouRate.insertMany(jobDocs);
+  return insertedDocs;
+};
+
+prefillDataForJobRates()
+  .then((success) => {
+    console.info('success', success);
+    process.exit();
+  })
+  .catch((err) => {
+    console.error('err', err);
+    process.exit();
+  });
