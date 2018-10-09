b9cb6a44891c4bfa174c11a3f575c1cee47bb5f4
G <igauravsehrawat@gmail.com>
2018-10-09 19:39:43 +0530

    --add: cors --refactor: routes

--- payroll-backend/app.js
+++ payroll-backend/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const index = require('./routes/index');
 const workLogReports = require('./routes/workLogReports');
@@ -16,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
+app.use(cors());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -24,8 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/workLogReports', workLogReports);
-app.use('/payrollReports', payrollReports);
+app.use('/worklog-reports', workLogReports);
+app.use('/payroll-reports', payrollReports);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
