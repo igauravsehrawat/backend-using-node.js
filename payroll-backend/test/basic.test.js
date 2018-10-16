74d5712c95e56849144af521cbae64d1fb53f43d
G <igauravsehrawat@gmail.com>
2018-10-16 22:30:06 +0530

    --update: test stack changed mocha + chai.

---
+++ payroll-backend/test/basic.test.js
@@ -0,0 +1,47 @@
+
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const server = require('../bin/www');
+
+const should = chai.should();
+const expect = chai.expect();
+
+chai.use(chaiHTTP);
+
+describe('If server is boots up', () => {
+  it('Render the homepage', (done) => {
+    chai.request(server).get('/')
+      .end((err, res) => {
+        console.log(res.body);
+        res.should.have.status(200);
+        res.text.should.include('Greeting');
+      });
+    done();
+  });
+});
+
+describe('Get payroll initially should have', () => {
+  it('Have empty data', (done) => {
+    chai.request(server).get('/payroll-reports/all')
+      .end((err, res) => {
+        console.log(res.body.data);
+        res.should.have.status(200);
+        res.body.data.should.be.a('array');
+        res.body.data.should.have.lengthOf(0);
+        done();
+      });
+  });
+});
+
+describe('Get payroll initially should have', () => {
+  it('Have empty data', (done) => {
+    chai.request(server).get('/payroll-reports/all')
+      .end((err, res) => {
+        console.log(res.body.data);
+        res.should.have.status(200);
+        res.body.data.should.be.a('array');
+        res.body.data.should.have.lengthOf(4);
+        done();
+      });
+  });
+});
