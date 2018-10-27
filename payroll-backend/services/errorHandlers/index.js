83cb39305a2f1e335554326c2eb5e09099ea262e
G <igauravsehrawat@gmail.com>
2018-10-27 12:40:54 +0530

    --fix: Bug fix, if using ES6 rest paramters, using spread operator too.

--- payroll-backend/services/errorHandlers/index.js
+++ payroll-backend/services/errorHandlers/index.js
@@ -13,7 +13,9 @@ const expressErrorHandler = fn => (req, res, next) => fn(req, res, next).catch((
   next({ err });
 });
 
-const errorHandler = fn => (...params) => fn(params).catch((err) => {
+// rest parameters
+// spread the arguments
+const errorHandler = fn => (...params) => fn(...params).catch((err) => {
   console.error(err);
   winstonLogger.error(err.err && err.err.stack);
   Sentry.captureException(err);
